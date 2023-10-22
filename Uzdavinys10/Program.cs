3164eac490d28f22622e210b8c9ffb92ab30bd5a
Edgaras Velička <edgarasvelicka84@gmail.com>
2023-10-22 12:27:47 +0300

    Uzdavinys10

---
+++ Uzdavinys10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uzdavinys10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.Write("Įveskite skaičių: "); int skaicius = Convert.ToInt32(Console.ReadLine());
+            if (skaicius % 2 == 0)
+            {
+                Console.WriteLine($"Įvestas skaičius: {skaicius}, pakeltas 5 laipsniu: {Math.Pow(skaicius, 5)}");
+            }
+            if (skaicius % 4 != 0)
+            {
+                Console.Write("Įveskite kitą skaičių:"); int skaitmuo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Skaičių: {skaicius} ir {skaitmuo} suma: {skaicius + skaitmuo} ir sandauga: {skaicius * skaitmuo}");
+            }
+            
+
+        }
+    }
+}
