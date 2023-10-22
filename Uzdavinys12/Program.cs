128887b4ac9677dec3fc17557cd4fa4fcfd8809f
Edgaras Velička <edgarasvelicka84@gmail.com>
2023-10-22 14:35:03 +0300

    Uzdavinys12

--- Uzdavinys12/Program.cs
+++ Uzdavinys12/Program.cs
@@ -11,24 +11,24 @@ namespace Uzdavinys12
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.Unicode;
-            //// 1 dalis
-            //Console.WriteLine("Įveskite tris skirtingus skaičius: ");
-            //Console.Write("Pirmas: "); double a = Convert.ToDouble(Console.ReadLine());
-            //Console.Write("Antras: "); double b = Convert.ToDouble(Console.ReadLine());
-            //Console.Write("Trečias: "); double c = Convert.ToDouble(Console.ReadLine());
-            //if (a > b && a > c)
-            //{ 
-            //    Console.WriteLine($"Skaičius {a} yra didesnis už {b} ir {c}");
-            //}
-            //else if  (b > a && b > c)
-            //{
-            //    Console.WriteLine($"Skaičius {b} yra didesnis už {a} ir {c}");
-            //}
-            //else if (c > a && c > b)
-            //{
-            //    Console.WriteLine($"Skaičius {c} yra didesnis už {a} ir {b}");
-            //}
-            //Console.WriteLine();
+            // 1 dalis
+            Console.WriteLine("Įveskite tris skirtingus skaičius: ");
+            Console.Write("Pirmas: "); double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Antras: "); double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Trečias: "); double c = Convert.ToDouble(Console.ReadLine());
+            if (a > b && a > c)
+            {
+                Console.WriteLine($"Skaičius {a} yra didesnis už {b} ir {c}");
+            }
+            else if (b > a && b > c)
+            {
+                Console.WriteLine($"Skaičius {b} yra didesnis už {a} ir {c}");
+            }
+            else if (c > a && c > b)
+            {
+                Console.WriteLine($"Skaičius {c} yra didesnis už {a} ir {b}");
+            }
+            Console.WriteLine();
 
             //  2 dalis 
             Console.Write("Įveskite egzamino rezultatą (nuo 1 iki 10): "); int rez = Convert.ToInt32(Console.ReadLine());
